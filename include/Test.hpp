5d91830613b6455a3ae2a680c8df4f7699d3c0d2
sefyan0hack <sefyanhack@gmail.com>
2024-09-27 22:16:37 +0100

    add auto regester test to my header test lib

--- include/Test.hpp
+++ include/Test.hpp
@@ -3,19 +3,32 @@
 #include <sstream>
 #include <cassert>
 #include <source_location>
+#include <type_traits>
+#include <concepts>
+#include <functional>
+#include <vector>
 
-#define Test(func) {\
+template <typename T>
+concept Streamable = requires(T a, std::ostream& os) {
+    { os << a } -> std::same_as<std::ostream&>;
+};
+#define T(Class, member) {\
         std::cout << std::endl;\
-        std::cout << "[" << __COUNTER__ + 1<< "] " << #func << " : ";\
-        func();\
+        std::cout << "[" << __COUNTER__ + 1<< "] " << #Class"."#member << " : ";\
+        Class##_##member();\
         std::cout << "SUCCESS" << std::endl;\
     }
 
-constexpr void EXPECT_EQ(auto val, decltype(val) expe){ 
+constexpr void EXPECT_EQ(auto val, decltype(val) expe){
     if (!(val == expe)) {
         std::ostringstream oss;
-        oss << "Expected: " << expe << ", but got: " << val;
-        std::cerr << oss.str() << std::endl;
+        if constexpr (Streamable<decltype(expe)>) {
+            oss << "Expected: " << expe << ", but got: " << val;
+            std::cerr << oss.str() << std::endl;
+        }
+        else{
+            std::cerr << "val and expected not Streamable " << std::endl;
+        }
         assert(false);
     }
 }
@@ -26,4 +39,29 @@ constexpr void EXPECT_NEQ(auto val, decltype(val) expe){
         std::cerr << oss.str() << std::endl;
         assert(false);
     }
-} 
+}
+
+using TestFunction = std::function<void()>;
+std::vector<TestFunction> rfuncs;
+
+std::vector<TestFunction>& getFuncRegistry() {
+    static std::vector<TestFunction> funcRegistry;
+    return funcRegistry;
+}
+
+void registerFunc(TestFunction func) {
+    getFuncRegistry().push_back(func);
+}
+
+class AutoRegistrar {
+public:
+    AutoRegistrar(TestFunction func) {
+        registerFunc(func);
+    }
+};
+#define Test(Class, member)           \
+    void Class##_##member();                        \
+    static AutoRegistrar _registrar_##Class##_##member([]() { T(Class, member); }); \
+    void Class##_##member()
+
+#define RUN_ALL_TESTS() {for (const auto& func : getFuncRegistry()) func();}
--- src/test.cpp
+++ src/test.cpp
@@ -1,7 +1,7 @@
 #include "Test.hpp"
 #include "List.hpp"
 
-void test_list_ctor(){
+Test(List, ctor){
     auto obj = List<int>{};
     EXPECT_EQ(obj.size(), 0);
 
@@ -10,7 +10,7 @@ void test_list_ctor(){
 
     EXPECT_EQ(obj2.str(), "{ [0] = 1, [1] = 1, [2] = 1, [3] = 1 }");
 }
-void test_list_push_back(){
+Test(List, push_back){
     auto obj = List<int>{};
     obj.push_back(1);
     obj.push_back(2);
@@ -18,7 +18,7 @@ void test_list_push_back(){
     EXPECT_EQ(obj.str(), "{ [0] = 1, [1] = 2 }");
     
 }
-void test_list_push_at(){
+Test(List, push_at){
     auto obj = List<int>{10, -1};
     EXPECT_EQ(obj.size(), 10);
     EXPECT_EQ(obj.str(), "{ [0] = -1, [1] = -1, [2] = -1, [3] = -1, [4] = -1, [5] = -1, [6] = -1, [7] = -1, [8] = -1, [9] = -1 }");
@@ -29,26 +29,26 @@ void test_list_push_at(){
 
 
 }
-void test_list_push_remove(){
+Test(List, remove){
     auto obj = List<int>{10, -1};
     EXPECT_EQ(obj.size(), 10);
     obj.remove(3);
     EXPECT_EQ(obj.size(), 9);
 }
-void test_list_clear(){
+Test(List, clear){
     auto obj = List<int>{10, -1};
     EXPECT_EQ(obj.size(), 10);
 
     obj.clear();
     EXPECT_EQ(obj.size(), 0);
 }
-void test_list_pop(){
+Test(List, pop){
     auto obj = List<int>{300, -1};
     EXPECT_EQ(obj.size(), 300);
     obj.pop();
     EXPECT_EQ(obj.size(), 300 - 1);
 }
-void test_list_front(){
+Test(List, front){
     auto obj = List<int>{2, -1};
     EXPECT_EQ(obj.size(), 2);
 
@@ -57,7 +57,7 @@ void test_list_front(){
     EXPECT_EQ(obj.front(), 11);
 }
 
-void test_list_op_pracket(){
+Test(List, op_pracket){
     auto obj = List<int>{5, -1};
     EXPECT_EQ(obj.size(), 5);
 
@@ -67,13 +67,13 @@ void test_list_op_pracket(){
     EXPECT_EQ(obj[2], 11);
 }
 
-void test_list_str(){
+Test(List, str){
     auto obj = List<int>{3, -1};
     EXPECT_EQ(obj.size(), 3);
 
     EXPECT_EQ(obj.str(), "{ [0] = -1, [1] = -1, [2] = -1 }");
 }
-void test_list_cctor(){
+Test(List, cctor){
     auto obj = List<int>{3, -1};
     EXPECT_EQ(obj.size(), 3);
     EXPECT_EQ(obj.str(), "{ [0] = -1, [1] = -1, [2] = -1 }");
@@ -82,7 +82,7 @@ void test_list_cctor(){
     EXPECT_EQ(obj2.size(), obj.size());
     EXPECT_EQ(obj2.str(), obj.str());
 }
-void test_list_op_cpy(){
+Test(List, op_cpy){
     auto obj = List<int>{3, -1};
     EXPECT_EQ(obj.str(), "{ [0] = -1, [1] = -1, [2] = -1 }");
 
@@ -109,17 +109,33 @@ void test_list_op_cpy(){
     EXPECT_EQ(obj4.size(), obj.size());
     EXPECT_EQ(obj4.str(), obj.str());
 }
+Test(List, find){
+    auto obj = List<int>();
+    obj.push_back(1);
+    obj.push_back(10);
+    obj.push_back(100);
+    obj.push_back(1000);
+
+    EXPECT_EQ(obj.find(10), 1);
+    EXPECT_EQ(obj.find(2), std::nullopt);
+}
+Test(List, iterators){
+    List<int> obj;
+    size_t i;
+    for (i = 0; i < 10; i++)
+    {
+        obj.push_back(i);
+    }
+
+    for (auto&& j : obj)
+    {
+        EXPECT_EQ(j, 10 - i--);
+    }
+
+    EXPECT_EQ(*obj.begin(), obj[0]);
+
+}
 
 int main(){
-    Test(test_list_ctor);
-    Test(test_list_push_back);
-    Test(test_list_push_at);
-    Test(test_list_push_remove);
-    Test(test_list_clear);
-    Test(test_list_pop);
-    Test(test_list_front);
-    Test(test_list_op_pracket);
-    Test(test_list_str);
-    Test(test_list_cctor);
-    Test(test_list_op_cpy);
+    RUN_ALL_TESTS();
 }
