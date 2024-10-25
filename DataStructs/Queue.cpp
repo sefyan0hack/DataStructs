61109ffcc4c7c4a4cf48b98552b5cf351c12e5db
sefyan0hack <sefyanhack@gmail.com>
2024-10-25 03:33:02 +0100

    ctor for Queue

--- DataStructs/Queue.cpp
+++ DataStructs/Queue.cpp
@@ -2,3 +2,32 @@
 #include "include/Queue.hpp"
 
 #define Queue_Init(t) template class sof::Queue<t>;
+using namespace sof;
+
+template <typename T>
+inline Queue<T>::Queue()
+    : Capacity(32),
+      m_size(0),
+      buffer(new T[Capacity * sizeof(value_type)]),
+      Head(&buffer[0]),
+      Tail(Head)
+{
+}
+
+template <typename T>
+inline Queue<T>::Queue(size_type size, const value_type& init) : Capacity(128), m_size(size)
+{
+    while (Capacity < size)
+    {
+        Capacity += Capacity / 2;
+    }
+
+    buffer = new T[Capacity * sizeof(value_type)];
+    Head   = &buffer[0];
+    Tail   = &buffer[size - 1];
+
+    for (size_type i = 0; i < size; i++)
+    {
+        buffer[i] = init;
+    }
+}
--- DataStructs/include/Queue.hpp
+++ DataStructs/include/Queue.hpp
@@ -2,11 +2,28 @@
 
 namespace sof
 {
-class Queue
-{
-public:
-    Queue(/* args */) {}
-    ~Queue() {}
-};
+    template <typename T>
+    class Queue
+    {
+       public:
+        using value_type      = T;
+        using reference       = value_type&;
+        using size_type       = size_t;
+        using const_reference = const value_type&;
+        using pointer         = T*;
+
+        Queue();
+        Queue(size_type size, const value_type& init);
+        ~Queue();
+
+       private:
+        size_type Capacity;
+        size_type m_size;
+        
+        T* buffer;
+        
+        T* Head;
+        T* Tail;
+    };
 
 }  // namespace sof
